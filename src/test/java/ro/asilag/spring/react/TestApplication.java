a0215f3e37e9013e71cc55bb509d1c61b37dd02a
asilag <liviu.savu@protonmail.com>
2025-07-06 19:00:46 +0300

    Initial commit

---
+++ src/test/java/ro/asilag/spring/react/TestApplication.java
@@ -0,0 +1,11 @@
+package ro.asilag.spring.react;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
